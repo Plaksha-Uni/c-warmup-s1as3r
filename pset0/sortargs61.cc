766fb0db00081e91910c980da48c70c1273c8a19
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2024-08-13 23:16:29 +0530

    part-6: use c++ stdlib for argsort

--- pset0/sortargs61.cc
+++ pset0/sortargs61.cc
@@ -1,8 +1,11 @@
+#include <algorithm>
 #include <cstddef>
-#include <cstdio>
 #include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
 
-void insertion_sort(char *arr[], size_t len) {
+[[maybe_unused]] void insertion_sort(char *arr[], size_t len) {
   for (size_t i = 1; i < len; i++) {
     size_t j = i;
     while (j > 0 && strcmp(arr[j - 1], arr[j]) > 0) {
@@ -14,10 +17,18 @@ void insertion_sort(char *arr[], size_t len) {
   }
 }
 
+std::vector<std::string> stdlib_sort(char *arr[], size_t len) {
+  std::vector<std::string> vec(arr, arr + len);
+
+  std::sort(vec.begin(), vec.end());
+
+  return vec;
+}
+
 int main(int argc, char *argv[]) {
-  insertion_sort(&argv[1], (size_t)argc - 1);
-  for (int i = 1; i < argc; i++) {
-    fprintf(stdout, "%s\n", argv[i]);
+  std::vector<std::string> vec = stdlib_sort(&argv[1], argc - 1);
+  for (std::string& elem : vec) {
+    std::cout << elem << "\n";
   }
   return 0;
 }
